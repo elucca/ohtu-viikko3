cdbd126e90a94af4ad0d37296de3bf432827230b
Juho Kesälä <elukka.elukka@gmail.com>
2018-11-15 19:30:14 +0200

    Tee tulostus, tehtävä 1 loppuun

--- Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,7 +1,18 @@
 package ohtu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Submission {
+
     private int week;
+    private int hours;
+    private List<Integer> exercises;
+    private String course;
+
+    public Submission() {
+        this.exercises = new ArrayList<>();
+    }
 
     public void setWeek(int week) {
         this.week = week;
@@ -11,9 +22,42 @@ public class Submission {
         return week;
     }
 
+    public int getHours() {
+        return hours;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public List<Integer> getExercises() {
+        return exercises;
+    }
+
+    public void setExercises(List<Integer> exercises) {
+        this.exercises = exercises;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
     @Override
     public String toString() {
-        return ""+week;
+        StringBuilder s = new StringBuilder(course + ", viikko " + week + " tehtyjä tehtäviä yhteensä " + exercises.size() + " aikaa kului " + hours + " tehdyt tehtävät: ");
+        for (int i = 0; i < exercises.size(); i++) {
+            if (i == exercises.size() - 1) {
+                s.append(exercises.get(i));
+            } else {
+                s.append(exercises.get(i) + (", "));
+            }
+        }
+
+        return s.toString();
     }
-    
+
 }
